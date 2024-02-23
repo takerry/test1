7c9c6a4bc3e4ef61b91672a7a0ba1d9830633e93
takerry <twinrael@gmail.com>
2024-02-23 12:23:35 +0700

    Initial commit

---
+++ src/Class.java
@@ -0,0 +1,12 @@
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class Class {
+    static PrintStream out = System.out;
+    static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        User user1 = new User(sc.nextLine(), sc.nextInt(), sc.nextLine().equalsIgnoreCase("male"));
+        out.println(" Hi! My name is "  + user1.name + ". I am " + user1.age + " old years");
+    }
+}
---
+++ src/User.java
@@ -0,0 +1,11 @@
+public class User {
+    String name;
+    int age;
+    boolean isMale;
+
+    public User(String name, int age, boolean isMale) {
+        this.name = name;
+        this.age = age;
+        this.isMale = isMale;
+    }
+}
